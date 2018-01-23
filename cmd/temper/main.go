562d9f776922924e70daf1733913443bdf8971b9
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-01-23 22:39:23 +0100

    Return DeviceTemperature structure instead of float

--- cmd/temper/main.go
+++ cmd/temper/main.go
@@ -10,7 +10,12 @@ func main() {
 	temp, err := temper.GetTemperature()
 
 	if err == nil {
-		log.Printf("Temperature: %.2f°K %.2f°F %.2f°C\n", temp+273.15, 9.0/5.0*temp+32, temp)
+		log.Printf(
+			"Temperature: %.2f°K %.2f°F %.2f°C\n",
+			temp.Temperature+273.15,
+			9.0/5.0*temp.Temperature+32,
+			temp.Temperature,
+		)
 	} else {
 		log.Fatalf("Failed: %s", err)
 	}
--- pkg/temper.go
+++ pkg/temper.go
@@ -7,10 +7,15 @@ import (
 	"github.com/google/gousb"
 )
 
+type DeviceTemperature struct {
+	Device      *gousb.Device
+	Temperature float64
+}
+
 // GetTemperature is a function that tries to find first USB TEMPer device on maching and read
 // the current temperature from there
 // Returns temperature in Celcius and error if it occured
-func GetTemperature() (float64, error) {
+func GetTemperature() (DeviceTemperature, error) {
 	ctx := gousb.NewContext()
 	defer ctx.Close()
 
@@ -27,21 +32,63 @@ func GetTemperature() (float64, error) {
 	}
 	if err != nil {
 		log.Fatalf("OpenDevices(): %v", err)
-		return 0.0, err
+		return DeviceTemperature{}, err
 	}
 	if len(devs) == 0 {
 		log.Fatalf("no devices found matching VID %s and PID %s", vid, pid)
-		return 0.0, fmt.Errorf("no devices found matching VID %s and PID %s", vid, pid)
+		return DeviceTemperature{}, fmt.Errorf("no devices found matching VID %s and PID %s", vid, pid)
 	}
 
 	// Pick the first device found.
 	dev := devs[0]
 
+	return getDeviceTemperature(dev)
+}
+
+// GetTemperatures is a function that tries to find all USB TEMPer devices on maching and read
+// the current temperatures from all devices
+// Returns temperatures in Celcius and error if it occured
+func GetTemperatures() ([]DeviceTemperature, error) {
+	ctx := gousb.NewContext()
+	defer ctx.Close()
+
+	// Iterate through available Devices, finding all that match a known VID/PID.
+	vid, pid := gousb.ID(0x0c45), gousb.ID(0x7401)
+	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
+		// this function is called for every device present.
+		// Returning true means the device should be opened.
+		return desc.Vendor == vid && desc.Product == pid
+	})
+
+	for _, d := range devs {
+		defer d.Close()
+	}
+	if err != nil {
+		log.Fatalf("OpenDevices(): %v", err)
+		return []DeviceTemperature{}, err
+	}
+	if len(devs) == 0 {
+		log.Fatalf("no devices found matching VID %s and PID %s", vid, pid)
+		return []DeviceTemperature{}, fmt.Errorf("no devices found matching VID %s and PID %s", vid, pid)
+	}
+
+	var temperatures []DeviceTemperature
+
+	for _, dev := range devs {
+		temp, err := getDeviceTemperature(dev)
+		if err == nil {
+			temperatures = append(temperatures, temp)
+		}
+	}
+	return temperatures, nil
+}
+
+func getDeviceTemperature(dev *gousb.Device) (DeviceTemperature, error) {
 	// Switch the configuration to #1.
 	cfg, err := dev.Config(1)
 	if err != nil {
 		log.Fatalf("%s.Config(1): %v", dev, err)
-		return 0.0, err
+		return DeviceTemperature{}, err
 	}
 	defer cfg.Close()
 
@@ -49,7 +96,7 @@ func GetTemperature() (float64, error) {
 	intf, err := cfg.Interface(1, 0)
 	if err != nil {
 		log.Fatalf("%s.Interface(1, 0): %v", cfg, err)
-		return 0.0, err
+		return DeviceTemperature{}, err
 	}
 	defer intf.Close()
 
@@ -57,7 +104,7 @@ func GetTemperature() (float64, error) {
 	epIn, err := intf.InEndpoint(0x82)
 	if err != nil {
 		log.Fatalf("%s.InEndpoint(0x82): %v", intf, err)
-		return 0.0, err
+		return DeviceTemperature{}, err
 	}
 
 	// Send device a control request with standard parameters and data
@@ -65,14 +112,14 @@ func GetTemperature() (float64, error) {
 		0x21, 0x09, 0x0200, 0x01, []byte{0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00},
 	)
 	if err != nil {
-		return 0.0, err
+		return DeviceTemperature{}, err
 	}
 
 	// Read response from USB temper device
 	buf := make([]byte, 8)
 	if _, err = epIn.Read(buf); err != nil {
-		return 0.0, err
+		return DeviceTemperature{}, err
 	}
 
-	return float64(buf[2]) + float64(buf[3])/256, nil
+	return DeviceTemperature{Device: dev, Temperature: float64(buf[2]) + float64(buf[3])/256}, nil
 }
